5e82f485c4384fa82ef60c40c952f260bb8655a3
pgunia <patrick.gunia@gmail.com>
2015-07-21 19:20:29 +0200

    Refactored EventFactory

--- src/com/soundcloud/followermaze/server/dispatcher/EventConnectionHandler.java
+++ src/com/soundcloud/followermaze/server/dispatcher/EventConnectionHandler.java
@@ -8,14 +8,11 @@ import org.apache.logging.log4j.Logger;
 
 import com.soundcloud.followermaze.server.config.ConfigService;
 import com.soundcloud.followermaze.server.service.EventHandlerService;
-import com.soundlcoud.followermaze.server.model.event.Event;
 import com.soundlcoud.followermaze.server.model.event.EventFactory;
 
 public class EventConnectionHandler extends ConnectionHandler {
 
   private static final Logger logger = LogManager.getLogger( EventConnectionHandler.class );
-
-  private static final String SEPARATOR = "\\|";
 
   public EventConnectionHandler( SocketChannel clientSocket ) {
     super( clientSocket );
@@ -32,35 +29,9 @@ public class EventConnectionHandler extends ConnectionHandler {
     message.get( byteBuffer, 0, numberOfBytesToRead );
 
     final String messageStr = new String( byteBuffer, "UTF-8" );
-    final String trimmedMessageStr = messageStr.trim();
-
     logger.info( "Processing message: " + messageStr );
 
-    // check for type based on String parsing
-    final String[] components = trimmedMessageStr.split( SEPARATOR );
-    final String eventTypeIdentifier = components[1];
-    Event result = null;
-    switch ( eventTypeIdentifier ) {
-      case "F":
-        result = EventFactory.createFollowEvent( messageStr );
-        break;
-      case "U":
-        result = EventFactory.createUnFollowEvent( messageStr );
-        break;
-      case "B":
-        result = EventFactory.createBroadcastEvent( messageStr );
-        break;
-      case "P":
-        result = EventFactory.createPrivateMessageEvent( messageStr );
-        break;
-      case "S":
-        result = EventFactory.createStatusUpdateEvent( messageStr );
-        break;
-      default:
-        throw new IllegalStateException( "Identifier: " + messageStr + " does not describe a valid event!" );
-    }
-
-    EventHandlerService.INSTANCE.addEvent( result );
+    EventHandlerService.INSTANCE.addEvent( EventFactory.createEvent( messageStr ) );
     logger.exit();
   }
 
--- src/com/soundlcoud/followermaze/server/model/event/EventFactory.java
+++ src/com/soundlcoud/followermaze/server/model/event/EventFactory.java
@@ -9,27 +9,66 @@ public class EventFactory {
 
   private static final String SEPARATOR = "\\|";
 
-  public static Event createFollowEvent( final String eventStr ) throws Exception {
+  /**
+   * Creates an event based on the input event message
+   * 
+   * @param eventStr
+   *          Received event message which is parsed and returns an Event instance
+   * @return Event instance representing the event to be processed
+   */
+  public static Event createEvent( final String eventStr ) {
     logger.entry( eventStr );
 
-    final String[] component = eventStr.trim().split( SEPARATOR );
+    // check for type based on String parsing, use trimmed component to remove linebreak at the end
+    final String[] components = eventStr.trim().split( SEPARATOR );
+    final String eventTypeIdentifier = components[1];
+    Event result = null;
+    try {
+      switch ( eventTypeIdentifier ) {
+        case "F":
+          result = createFollowEvent( components, eventStr );
+          break;
+        case "U":
+          result = createUnFollowEvent( components, eventStr );
+          break;
+        case "B":
+          result = createBroadcastEvent( components, eventStr );
+          break;
+        case "P":
+          result = createPrivateMessageEvent( components, eventStr );
+          break;
+        case "S":
+          result = createStatusUpdateEvent( components, eventStr );
+          break;
+        default:
+          throw new IllegalStateException( "Identifier: " + eventStr + " does not describe a valid event!" );
+      }
+    } catch ( Exception ex ) {
+      logger.error( "Error during event creation", ex );
+    }
+    logger.exit( result );
+    return result;
+
+  }
+
+  private static Event createFollowEvent( final String[] component, final String message ) throws Exception {
+    logger.entry( component, message );
 
     if ( component.length != 4 ) {
-      throw new IllegalArgumentException( "Event " + eventStr + " does not describe a valid follow event." );
+      throw new IllegalArgumentException( "Event " + message + " does not describe a valid follow event." );
     }
     final int sequenceNumber = extractSequenceNumber( component[0] );
     final int clientIdFrom = extractClientId( component[2] );
     final int clientIdTo = extractClientId( component[3] );
-    final Event result = new FollowEvent( sequenceNumber, clientIdFrom, clientIdTo, eventStr );
+    final Event result = new FollowEvent( sequenceNumber, clientIdFrom, clientIdTo, message );
     logger.exit( result );
     return result;
   }
 
-  public static Event createUnFollowEvent( final String eventStr ) throws Exception {
-    logger.entry( eventStr );
-    final String[] component = eventStr.trim().split( SEPARATOR );
+  private static Event createUnFollowEvent( final String[] component, final String message ) throws Exception {
+    logger.entry( component, message );
     if ( component.length != 4 ) {
-      throw new IllegalArgumentException( "Event " + eventStr + " does not describe a valid unfollow event." );
+      throw new IllegalArgumentException( "Event " + message + " does not describe a valid unfollow event." );
     }
 
     final int sequenceNumber = extractSequenceNumber( component[0] );
@@ -40,47 +79,44 @@ public class EventFactory {
     return result;
   }
 
-  public static Event createBroadcastEvent( final String eventStr ) throws Exception {
-    logger.entry( eventStr );
-    final String[] component = eventStr.trim().split( SEPARATOR );
+  private static Event createBroadcastEvent( final String[] component, final String message ) throws Exception {
+    logger.entry( component, message );
 
     if ( component.length != 2 ) {
-      throw new IllegalArgumentException( "Event " + eventStr + " does not describe a valid broadcast event." );
+      throw new IllegalArgumentException( "Event " + message + " does not describe a valid broadcast event." );
     }
 
     final int sequenceNumber = extractSequenceNumber( component[0] );
-    final Event result = new BroadcastEvent( sequenceNumber, eventStr );
+    final Event result = new BroadcastEvent( sequenceNumber, message );
     logger.exit( result );
     return result;
   }
 
-  public static Event createPrivateMessageEvent( final String eventStr ) throws Exception {
-    logger.entry( eventStr );
-    final String[] component = eventStr.trim().split( SEPARATOR );
+  private static Event createPrivateMessageEvent( final String[] component, final String message ) throws Exception {
+    logger.entry( component, message );
 
     if ( component.length != 4 ) {
-      throw new IllegalArgumentException( "Event " + eventStr + " does not describe a valid private message event." );
+      throw new IllegalArgumentException( "Event " + message + " does not describe a valid private message event." );
     }
 
     final int sequenceNumber = extractSequenceNumber( component[0] );
     final int clientIdFrom = extractClientId( component[2] );
     final int clientIdTo = extractClientId( component[3] );
-    final Event result = new PrivateMsgEvent( sequenceNumber, clientIdFrom, clientIdTo, eventStr );
+    final Event result = new PrivateMsgEvent( sequenceNumber, clientIdFrom, clientIdTo, message );
     logger.exit( result );
     return result;
   }
 
-  public static Event createStatusUpdateEvent( final String eventStr ) throws Exception {
-    logger.entry( eventStr );
-    final String[] component = eventStr.trim().split( SEPARATOR );
+  private static Event createStatusUpdateEvent( final String[] component, final String message ) throws Exception {
+    logger.entry( component, message );
 
     if ( component.length != 3 ) {
-      throw new IllegalArgumentException( "Event " + eventStr + " does not describe a valid status update event." );
+      throw new IllegalArgumentException( "Event " + message + " does not describe a valid status update event." );
     }
 
     final int sequenceNumber = extractSequenceNumber( component[0] );
     final int clientIdFrom = extractClientId( component[2] );
-    final Event result = new StatusUpdateEvent( sequenceNumber, clientIdFrom, eventStr );
+    final Event result = new StatusUpdateEvent( sequenceNumber, clientIdFrom, message );
     logger.exit( result );
     return result;
   }
